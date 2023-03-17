7e2cbe62818ddf867187804f6a365d2d03a8fe5d
Maxime <maxime.rache@gmail.com>
2023-03-17 12:55:49 +0100

    Ajout fonctions

--- pieces.js
+++ pieces.js
@@ -26,7 +26,7 @@ for (let i = 0; i < (pieces.length); i++) {
     sectionFiches.appendChild(pieceElement);
 }
 
-const boutonTrier = document.querySelector('.btn-trier');
+const boutonTrier = document.querySelector('.btn-trier'); /* Tri par pri croissant */
 
 boutonTrier.addEventListener("click", function() {
 
@@ -39,7 +39,7 @@ boutonTrier.addEventListener("click", function() {
     console.log(piecesOrdonnees);
 });
 
-const boutonFiltrer = document.querySelector('.btn-filtrer');
+const boutonFiltrer = document.querySelector('.btn-filtrer'); /* Tri des éléments non abordables */
 
 boutonFiltrer.addEventListener("click", function() {
 
@@ -50,7 +50,7 @@ boutonFiltrer.addEventListener("click", function() {
     console.log(piecesFiltrees);
 });
 
-const boutonDescription = document.querySelector('.btn-nodesc');
+const boutonDescription = document.querySelector('.btn-nodesc'); /* Tri des éléments ne possédant pas une description */
 
 boutonDescription.addEventListener("click", function() {
 
@@ -61,7 +61,7 @@ boutonDescription.addEventListener("click", function() {
     console.log(piecesFiltrees);
 });
 
-const boutonDecroissant = document.querySelector('.btn-decroissant');
+const boutonDecroissant = document.querySelector('.btn-decroissant'); /* Tri par pri décroissant */
 
 boutonDecroissant.addEventListener("click", function() {
 
@@ -73,3 +73,42 @@ boutonDecroissant.addEventListener("click", function() {
 
     console.log(piecesOrdonnees);
 });
+
+const boutonMap = document.querySelector('.btn-map'); /* Tri pour ne récupérer que les noms des pièces*/
+
+boutonMap.addEventListener("click", function() {
+    const nomsPieces = pieces.map(piece => piece.nom);
+    console.log(nomsPieces);
+});
+
+const boutonMapTri = document.querySelector('.btn-mapTri'); /* Affichage des noms des pièces abordables */
+
+const abordables = document.querySelector(".abordables"); 
+abordables.style.display = "none";
+
+
+boutonMapTri.addEventListener("click", function() {
+    
+    const noms = pieces.map(piece => piece.nom);
+
+    for(let i = pieces.length - 1; i >= 0; i--) {
+        if(pieces[i].prix >= 35) {
+            noms.splice(i, 1);
+        }
+    }
+
+    const elementsAbordables = document.createElement("ul");
+
+    for(let i = 0; i < noms.length; i++) {
+        const elements = document.createElement("li");
+        elements.innerText = noms[i];
+        elementsAbordables.appendChild(elements);
+    }
+
+    abordables.appendChild(elementsAbordables);
+    abordables.style.display = "inline-block";
+    console.log(noms);
+});
+
+const elementsDisponibles = pieces.map(piece => piece.nom, piece => piece.description);
+console.log(elementsDisponibles);
